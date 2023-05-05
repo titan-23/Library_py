d5f37890f37c4a1564a161727bd37c5dd97992a7
titanium-22 <titan.kaneda@gmail.com>
2023-05-05 14:05:57 +0900

    edit static rmq

--- DataStructures/StaticArrayQuery/StaticRmQ.py
+++ DataStructures/StaticArrayQuery/StaticRmQ.py
@@ -5,7 +5,7 @@ class StaticRmQ():
 
   class SparseTableRmQ():
 
-    def __init__(self, a: List[int], e: int):
+    def __init__(self, a: List[int], INF: int):
       self.size = len(a)
       log = self.size.bit_length()-1
       self.data = [a] + [[]] * log
@@ -13,22 +13,22 @@ class StaticRmQ():
         pre = self.data[i]
         l = 1 << i
         self.data[i+1] = [min(pre[j], pre[j+l]) for j in range(len(pre)-l)]
-      self.e = e
+      self.INF = INF
 
     def prod(self, l: int, r: int) -> int:
-      if l >= r: return self.e
+      if l >= r: return self.INF
       u = (r-l).bit_length()-1
       return min(self.data[u][l], self.data[u][r-(1<<u)])
 
-  def __init__(self, a: Iterable[int], e=10**9):
-    self.INF = e
+  def __init__(self, a: Iterable[int], INF=10**9):
     a = list(a)
     n = len(a)
     bucket_size = n.bit_length()//2 + 1
+    bucket_size = 31
     bucket_cnt = (n + bucket_size - 1) // bucket_size
     bucket = [a[bucket_size*i:bucket_size*(i+1)] for i in range(bucket_cnt)]
-    bucket_data = StaticRmQ.SparseTableRmQ([min(b) for b in bucket], self.INF)
-    bucket_bit = [array('I', bytes(4*bucket_size)) for _ in range(bucket_cnt)]
+    bucket_data = StaticRmQ.SparseTableRmQ([min(b) for b in bucket], INF)
+    bucket_bit = array('I', bytes(4*bucket_size*bucket_cnt))
 
     for i, b in enumerate(bucket):
       stack = []
@@ -40,8 +40,9 @@ class StaticRmQ():
           g = stack[-1]
         stack.append(j)
         if g == -1: continue
-        bucket_bit[i][j] = bucket_bit[i][g] | (1 << g)
+        bucket_bit[i*bucket_size+j] = bucket_bit[i*bucket_size+g] | (1 << g)
     self.n = n
+    self.INF = INF
     self.bucket_size = bucket_size
     self.bucket_data = bucket_data
     self.bucket = bucket
@@ -57,15 +58,15 @@ class StaticRmQ():
     l -= k1 * self.bucket_size
     r -= k2 * self.bucket_size + 1
     if k1 == k2:
-      bit = self.bucket_bit[k1][r] >> l
+      bit = self.bucket_bit[k1*self.bucket_size+r] >> l
       return self.bucket[k1][(bit&(-bit)).bit_length()+l-1 if bit else r]
     ans = self.INF
     if l < len(self.bucket[k1]):
-      bit = self.bucket_bit[k1][-1] >> l
+      bit = self.bucket_bit[k1*self.bucket_size+len(self.bucket[k1])-1] >> l
       ans = self.bucket[k1][(bit&(-bit)).bit_length()+l-1 if bit else -1]
     ans = min(ans, self.bucket_data.prod(k1+1, k2))
     if r >= 0:
-      bit = self.bucket_bit[k2][r]
+      bit = self.bucket_bit[k2*self.bucket_size+r]
       ans = min(ans, self.bucket[k2][(bit&(-bit)).bit_length()-1 if bit else r])
     return ans
 
