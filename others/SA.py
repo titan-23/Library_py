9eecc5686dfe525e0d481442b93149ece1887b8f
titanium-22 <titan.kaneda@gmail.com>
2023-07-20 10:41:43 +0900

    edit yakinamashi

--- others/SA.py
+++ others/SA.py
@@ -6,38 +6,41 @@ from typing import Tuple
 
 random.seed(0)
 
-START_TIME = time()
-TIME_LIMIT = 1.8
-END_TEMP = 10
-START_TEMP = 100
+class SA():
 
-def make_ans_init() -> Tuple:
-  return
+  def __init__(self):
+    self.END_TEMP = 10
+    self.START_TEMP = 100
 
-def modify(ans) -> Tuple:
-  return
+  def make_ans_init(self) -> Tuple:
+    return
 
-def rollback(ans, changed) -> None:
-  return
+  def modify(self, ans) -> Tuple:
+    return
 
-def SA_solver() -> Tuple:
-  ans, score = make_ans_init()
-  vest_ans = deepcopy(ans)
-  vest_score = score
-  cnt = 0
-  while True:
-    now_time = time() - START_TIME
-    if now_time > TIME_LIMIT: break
-    cnt += 1
-    diff_score, changed = modify(ans)
-    new_score = score + diff_score
-    temp = START_TEMP + (END_TEMP - START_TEMP) * now_time / TIME_LIMIT
-    arg = new_score - score
-    if 1 if arg >= 1 else exp(arg/temp) > random.random():
-      score = new_score
-      if new_score > vest_score:
-        vest_score = new_score
-        vest_ans = deepcopy(ans)
-    else:
-      rollback(ans, changed)
-  return vest_ans, vest_score, cnt
+  def rollback(self, ans, changed) -> None:
+    return
+
+  def sovle(self, TIME_LIMIT=1.8) -> Tuple:
+    START_TIME = time()
+    START_TEMP, END_TEMP = self.START_TEMP, self.END_TEMP
+    ans, score = self.make_ans_init()
+    vest_ans = deepcopy(ans)
+    vest_score = score
+    cnt = 0
+    while True:
+      now_time = time() - START_TIME
+      if now_time > TIME_LIMIT: break
+      cnt += 1
+      diff_score, changed = self.modify(ans)
+      new_score = score + diff_score
+      temp = START_TEMP + (END_TEMP - START_TEMP) * now_time / TIME_LIMIT
+      arg = new_score - score
+      if 1 if arg >= 1 else exp(arg/temp) > random.random():
+        score = new_score
+        if new_score > vest_score:
+          vest_score = new_score
+          vest_ans = deepcopy(ans)
+      else:
+        self.rollback(ans, changed)
+    return vest_ans, vest_score, cnt
