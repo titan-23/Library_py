ca5e110f07f0126c6033b81c33292a087c11eb1a
titanium-22 <titan.kaneda@gmail.com>
2023-09-13 16:17:53 +0900

    [edit] Persistent

--- DataStructures/PersistentStructure/PartialPersistentUnionFind.py
+++ DataStructures/PersistentStructure/PartialPersistentUnionFind.py
@@ -4,9 +4,11 @@ class PartialPersistentUnionFind():
 
   def __init__(self, n: int):
     self._n: int = n
-    self._parents: PartialPersistentArray = PartialPersistentArray([-1] * n)
+    self._parents: PartialPersistentArray[int] = PartialPersistentArray([-1] * n)
+    self._last_time: int = 0
 
   def root(self, x: int, t: int=-1) -> int:
+    assert t == -1 or t <= self._last_time
     while True:
       p = self._parents.get(x, t)
       if p < 0:
@@ -14,6 +16,8 @@ class PartialPersistentUnionFind():
       x = p
 
   def unite(self, x: int, y: int, t: int) -> bool:
+    assert t == -1 or t >= self._last_time
+    self._last_time = t
     x = self.root(x, t)
     y = self.root(y, t)
     if x == y:
@@ -25,8 +29,10 @@ class PartialPersistentUnionFind():
     return True
 
   def size(self, x: int, t: int=-1) -> int:
+    assert t == -1 or t <= self._last_time
     return -self._parents.get(self.root(x, t), t)
 
   def same(self, x: int, y: int, t: int=-1) -> bool:
+    assert t == -1 or t <= self._last_time
     return self.root(x, t) == self.root(y, t)
 
--- DataStructures/PersistentStructure/PersistentArray.py
+++ DataStructures/PersistentStructure/PersistentArray.py
@@ -1,20 +1,21 @@
-from typing import Any, Iterable, List
+from typing import Iterable, List, TypeVar, Generic
+T = TypeVar('T')
 
-class PartialPersistentArray():
+class PartialPersistentArray(Generic[T]):
 
-  def __init__(self, a: Iterable[Any]):
-    self.a = [[e] for e in a]
-    self.t: List[List[int]] = [[-1] for _ in range(len(self.a))]
+  def __init__(self, a: Iterable[T]):
+    self.a: List[List[T]] = [[e] for e in a]
+    self.t: List[List[int]] = [[0] for _ in range(len(self.a))]
     self.last_time: int = 0
 
-  def set(self, k: int, v: int, t: int) -> None:
+  def set(self, k: int, v: T, t: int) -> None:
     assert t >= self.last_time
     assert t > self.t[k][-1]
     self.a[k].append(v)
     self.t[k].append(t)
     self.last_time = t
-  
-  def get(self, k: int, t: int=-1) -> Any:
+
+  def get(self, k: int, t: int=-1) -> T:
     if t == -1 or t >= self.t[k][-1]:
       return self.a[k][-1]
     tk = self.t[k]
@@ -26,13 +27,16 @@ class PartialPersistentArray():
       else:
         ng = mid
     return self.a[k][ok]
-  
+
+  def tolist(self, t: int) -> List[T]:
+    return [self.get(i, t) for i in range(len(self))]
+
   def show(self, t: int) -> None:
+    print(f'Time: {t}', end='')
     print([self.get(i, t) for i in range(len(self))])
 
   def show_all(self) -> None:
     for i in range(self.last_time):
-      print(f'Time: {i}', end='')
       self.show(i)
 
   def __len__(self):
