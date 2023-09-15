4001ba317eb15f29cf6f7a0dd7d62edb37c1bc88
titanium-22 <titan.kaneda@gmail.com>
2023-09-15 13:02:06 +0900

    [Edit] doubling

--- Algorithm/Doubling.py
+++ Algorithm/Doubling.py
@@ -19,7 +19,7 @@ class Doubling(Generic[T]):
         db[k+1][i] = db[k][db[k][i]]
     self.db = db
 
-  def kth(self, start: T, k: int):
+  def kth(self, start: T, k: int) -> T:
     now = start
     for i in range(self.log):
       if k & 1:
---
+++ docs/Algorithm/Algorithm.md
@@ -0,0 +1,7 @@
+_____
+
+# [Algorithm](https://github.com/titanium-22/Library_py/tree/main/Algorithm)
+
+_____
+
+- Algorithm 系のライブラリです。
