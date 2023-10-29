bbce1e22b9f11f2ed42d64e8d107f8803a654828
titanium-22 <titan.kaneda@gmail.com>
2023-10-29 15:13:05 +0900

    [Edit] 色々

--- Algorithm/Random/RandomTree.py
+++ Algorithm/Random/RandomTree.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from Library_py.DataStructures.AVLTree.AVLTreeMultiset import AVLTreeMultiset
+from typing import Optional, List, Tuple
 import random
 
 class RandomTree():
@@ -7,7 +8,23 @@ class RandomTree():
     self.n = n
     random.seed(seed)
 
-  def build(self):
+  def build(self, com='') -> List[Tuple[int, int]]:
+    if com == '' or com == 'random':
+      return self._build_random()
+    if com == 'path':
+      return self._build_path()
+    raise ValueError(com)
+  
+  def _build_path(self) -> List[Tuple[int, int]]:
+    p = list(range(self.n))
+    random.shuffle(p)
+    Edge = []
+    for i in range(self.n-1):
+      Edge.append((p[i], p[i+1]))
+    random.shuffle(Edge)
+    return Edge
+
+  def _build_random(self) -> List[Tuple[int, int]]:
     Edge = []
     D = [1] * self.n
     A = [0] * (self.n-2)
@@ -15,20 +32,32 @@ class RandomTree():
       v = random.randrange(0, self.n)
       D[v] += 1
       A[i] = v
-    start = 0
+    # start = 0
+    # for a in A:
+    #   v = self.n
+    #   for j in range(start, self.n):
+    #     if D[j] == 1:
+    #       v = j
+    #       break
+    #     if D[j] < 1 and start == j-1:
+    #       start = j
+    #   else:
+    #     assert False
+    #   Edge.append((v, a))
+    #   D[v] -= 1
+    #   D[a] -= 1
+
+    avl = AVLTreeMultiset((D[i], i) for i in range(self.n))
     for a in A:
-      v = self.n
-      for j in range(start, self.n):
-        if D[j] == 1:
-          v = j
-          break
-        if D[j] < 1 and start == j-1:
-          start = j
-      else:
-        assert False
+      d, v = avl.pop_min()
+      assert d == 1
       Edge.append((v, a))
       D[v] -= 1
+      avl.remove((D[a], a))
       D[a] -= 1
+      if D[a] >= 1:
+        avl.add((D[a], a))
+
     u = D.index(1)
     D[u] -= 1
     v = D.index(1)
