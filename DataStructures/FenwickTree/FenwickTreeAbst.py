aeb67fcd6e82e674e1adc2a0e20d27ee5a68b184
titanium-22 <titan.kaneda@gmail.com>
2023-12-03 13:59:58 +0900

    [Edit] fw abst

--- DataStructures/FenwickTree/FenwickTreeAbst.py
+++ DataStructures/FenwickTree/FenwickTreeAbst.py
@@ -20,7 +20,6 @@ class FenwickTreeAbst(Generic[T]):
     self._s = 1 << (self._size - 1).bit_length()
 
   def pref(self, r: int) -> T:
-    '''Return sum(a[0, r)) / O(logN)'''
     assert 0 <= r <= self._size, \
         f'IndexError: FenwickTreeAbst.pref({r}), n={self._size}'
     ret = self.e
@@ -30,16 +29,24 @@ class FenwickTreeAbst(Generic[T]):
     return ret
 
   def suff(self, l: int) -> T:
-    '''Return sum(a[l, n)). / O(logN)'''
     assert 0 <= l < self._size, \
         f'IndexError: FenwickTreeAbst.suff({l}), n={self._size}'
     return self.op(self.pref(self._size), self.inv(self.pref(l)))
 
   def sum(self, l: int, r: int) -> T:
-    '''Return sum(a[l, r)]. / O(logN)'''
     assert 0 <= l <= r <= self._size, \
         f'IndexError: FenwickTreeAbst.sum({l}, {r}), n={self._size}'
-    return self.op(self.pref(r), self.inv(self.pref(l)))
+    _tree = self._tree
+    res = self.e
+    while r > l:
+      res = self.op(res, _tree[r])
+      r &= r - 1
+    while l > r:
+      res += self.inv(_tree[l])
+      l &= l - 1
+    return res
+
+  prod = sum
 
   def __getitem__(self, k: int) -> T:
     assert -self._size <= k < self._size, \
@@ -48,7 +55,6 @@ class FenwickTreeAbst(Generic[T]):
     return self.op(self.pref(k+1), self.inv(self.pref(k)))
 
   def add(self, k: int, x: T) -> None:
-    '''Add x to a[k]. / O(logN)'''
     assert 0 <= k < self._size, \
         f'IndexError: FenwickTreeAbst.add({k}, {x}), n={self._size}'
     k += 1
@@ -57,7 +63,6 @@ class FenwickTreeAbst(Generic[T]):
       k += k & -k
 
   def __setitem__(self, k: int, x: T):
-    '''Update A[k] to x. / O(logN)'''
     assert -self._size <= k < self._size, \
         f'IndexError: FenwickTreeAbst.__setitem__({k}, {x}), n={self._size}'
     if k < 0: k += self._size
@@ -73,14 +78,4 @@ class FenwickTreeAbst(Generic[T]):
 
   def __repr__(self):
     return f'FenwickTreeAbst({self})'
-
-# def op(s, t):
-#   # return s + t
-#   return
-
-# def inv(s):
-#   # return -s
-#   return
-
-# e = None
 
