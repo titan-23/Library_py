f26e56746fcf261ef5473b68611117506c4a1703
titanium-22 <titan.kaneda@gmail.com>
2024-01-01 16:18:23 +0900

    [Edit] docs

--- a.sh
+++
@@ -1,3 +0,0 @@
-eval $"sphinx-apidoc -f -e -d 1 -o ./docs/titan_pylib_docs ./titan_pylib/"
-eval $"sphinx-build -b html ./docs ./docs/_build"
-eval $"pypy3 ./a.py"
--- titan_pylib/data_structures/avl_tree/__init__.py
+++
@@ -1,3 +0,0 @@
-# __all__ = [
-#     'avltree_set',
-# ]
