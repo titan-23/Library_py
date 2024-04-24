fc4802b2b1f7dee7987f83604c7ffa926aec9aff
titanium-22 <titan.kaneda@gmail.com>
2024-04-25 03:50:19 +0900

    [Add] setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+  name="titan_pylib",
+  version="0.1.0",
+  license="ライセンス",
+  description="パッケージの説明",
+  author="titan23",
+  url="https://github.com/titan-23/Library_py",
+  packages=find_packages("titan_pylib"),
+  package_dir={"": "titan_pylib"},
+  py_modules=[splitext(basename(path))[0] for path in glob('titan_pylib/*.py')],
+  include_package_data=True,
+  zip_safe=False,
+)
