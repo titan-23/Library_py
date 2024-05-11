4180e2ba77353926689bd811b4eba1107cd04b5b
titanium-22 <titan.kaneda@gmail.com>
2024-05-12 03:06:29 +0900

    teset

--- titan_pylib/data_structures/wb_Tree/wbt_list.py
+++ titan_pylib/data_structures/wb_Tree/wbt_list.py
@@ -1,223 +1,8 @@
-from typing import Generic, TypeVar, Optional, Final
-
-T = TypeVar("T")
-
-
-class _WBTNodeBase(Generic[T]):
-    """WBTノードのベースクラス
-    size, par, left, rightをもつ
-    """
-
-    __slots__ = "_size", "_par", "_left", "_right"
-    DELTA: Final[int] = 3
-    GAMMA: Final[int] = 2
-
-    def __init__(self) -> None:
-        self._size: int = 1
-        self._par: Optional[_WBTNodeBase[T]] = None
-        self._left: Optional[_WBTNodeBase[T]] = None
-        self._right: Optional[_WBTNodeBase[T]] = None
-
-    def _weight_left(self) -> int:
-        return self._left._size + 1 if self._left else 1
-
-    def _weight_right(self) -> int:
-        return self._right._size + 1 if self._right else 1
-
-    def _update(self) -> None:
-        self._size = (
-            1
-            + (self._left._size if self._left else 0)
-            + (self._right._size if self._right else 0)
-        )
-
-    def _rotate_right(self) -> "_WBTNodeBase[T]":
-        u = self._left
-        u._size = self._size
-        self._size -= u._left._size + 1 if u._left else 1
-        u._par = self._par
-        self._left = u._right
-        if u._right:
-            u._right._par = self
-        u._right = self
-        self._par = u
-        if u._par:
-            if u._par._left is self:
-                u._par._left = u
-            else:
-                u._par._right = u
-        return u
-
-    def _rotate_left(self) -> "_WBTNodeBase[T]":
-        u = self._right
-        u._size = self._size
-        self._size -= u._right._size + 1 if u._right else 1
-        u._par = self._par
-        self._right = u._left
-        if u._left:
-            u._left._par = self
-        u._left = self
-        self._par = u
-        if u._par:
-            if u._par._left is self:
-                u._par._left = u
-            else:
-                u._par._right = u
-        return u
-
-    def _balance_check(self) -> None:
-        if not self._weight_left() * _WBTNodeBase.DELTA >= self._weight_right():
-            print(self._weight_left(), self._weight_right(), flush=True)
-            print(self)
-            assert False, f"self._weight_left() * DELTA >= self._weight_right()"
-        if not self._weight_right() * _WBTNodeBase.DELTA >= self._weight_left():
-            print(self._weight_left(), self._weight_right(), flush=True)
-            print(self)
-            assert False, f"self._weight_right() * DELTA >= self._weight_left()"
-
-
+from titan_pylib.data_structures.wb_tree.wbt_list_node import _WBTListNode
 from typing import Generic, TypeVar, Optional, Iterable, Callable
 
 T = TypeVar("T")
 F = TypeVar("F")
-
-
-class _WBTListNode(_WBTNodeBase, Generic[T, F]):
-
-    __slots__ = (
-        "_left",
-        "_right",
-        "_par",
-        "_tree",
-        "_key",
-        "_lazy",
-        "_data",
-        "_rdata",
-        "_rev",
-    )
-
-    def __init__(self, tree: "WBTList[T, F]", key: T, lazy: F) -> None:
-        super().__init__()
-        self._tree = tree
-        self._key: T = key
-        self._data: T = key
-        self._rdata: T = key
-        self._lazy: F = lazy
-        self._rev: int = 0
-
-        self._left: "_WBTListNode[T]"
-        self._right: "_WBTListNode[T]"
-        self._par: "_WBTListNode[T]"
-
-    def __str__(self) -> str:
-        if self._left is None and self._right is None:
-            return f"key:{self._key, self._size}\n"
-        return f"key:{self._key, self._size},\n _left:{self._left},\n _right:{self._right}\n"
-
-    def check(self):
-        def dfs(node: "_WBTListNode"):
-            s = 1
-            if node._left:
-                assert node._left._par is node
-                s += node._left._size
-                dfs(node._left)
-            if node._right:
-                assert node._right._par is node
-                s += node._right._size
-                dfs(node._right)
-            assert s == node._size
-
-        dfs(self)
-        # print("check ok.")
-
-    def _update(self) -> None:
-        self._size = 1
-        self._data = self._key
-        self._rdata = self._key
-        if self._left:
-            self._size += self._left._size
-            self._data = self._tree._op(self._left._data, self._data)
-            self._rdata = self._tree._op(self._rdata, self._left._rdata)
-        if self._right:
-            self._size += self._right._size
-            self._data = self._tree._op(self._data, self._right._data)
-            self._rdata = self._tree._op(self._right._rdata, self._rdata)
-
-    def _apply_rev(self) -> None:
-        self._rev ^= 1
-
-    def _apply_lazy(self, f: F) -> None:
-        self._key = self._tree._mapping(f, self._key)
-        self._data = self._tree._mapping(f, self._data)
-        self._rdata = self._tree._mapping(f, self._rdata)
-        self._lazy = self._tree._composition(f, self._lazy)
-
-    def _propagate(self) -> None:
-        if self._rev:
-            self._data, self._rdata = self._rdata, self._data
-            self._left, self._right = self._right, self._left
-            if self._left:
-                self._left._apply_rev()
-            if self._right:
-                self._right._apply_rev()
-            self._rev = 0
-        if self._lazy != self._tree._id:
-            if self._left:
-                self._left._apply_lazy(self._lazy)
-            if self._right:
-                self._right._apply_lazy(self._lazy)
-            self._lazy = self._tree._id
-
-    def __rotate_right(self) -> "_WBTListNode[T, F]":
-        u = self._left
-        u._propagate()
-        u._par = self._par
-        self._left = u._right
-        if u._right:
-            u._right._par = self
-        u._right = self
-        self._par = u
-        if u._par:
-            if u._par._left is self:
-                u._par._left = u
-            else:
-                u._par._right = u
-        self._update()
-        u._update()
-        return u
-
-    def __rotate_left(self) -> "_WBTListNode[T, F]":
-        u = self._right
-        u._propagate()
-        u._par = self._par
-        self._right = u._left
-        if u._left:
-            u._left._par = self
-        u._left = self
-        self._par = u
-        if u._par:
-            if u._par._left is self:
-                u._par._left = u
-            else:
-                u._par._right = u
-        self._update()
-        u._update()
-        return u
-
-    def _balance_left(self) -> "_WBTListNode[T, F]":
-        self._right._propagate()
-        if (
-            self._right._weight_left()
-            >= self._right._weight_right() * _WBTNodeBase.GAMMA
-        ):
-            self._right = self._right.__rotate_right()
-        return self.__rotate_left()
-
-    def _balance_right(self) -> "_WBTListNode[T, F]":
-        self._left._propagate()
-        if self._left._weight_right() >= self._left._weight_left() * _WBTNodeBase.GAMMA:
-            self._left = self._left.__rotate_left()
-        return self.__rotate_right()
 
 
 class WBTList(Generic[T, F]):
@@ -240,8 +25,7 @@ class WBTList(Generic[T, F]):
         self._id = id_
         self.__build(a)
 
-    def __build(self, a):
-
+    def __build(self, a: Iterable[T]) -> None:
         def build(l: int, r: int, pnode: Optional[_WBTListNode] = None) -> _WBTListNode:
             if l == r:
                 return None
@@ -253,7 +37,8 @@ class WBTList(Generic[T, F]):
             node._update()
             return node
 
-        a = list(a)
+        if not isinstance(a, list):
+            a = list(a)
         if not a:
             return
         self._root = build(0, len(a))
@@ -302,6 +87,7 @@ class WBTList(Generic[T, F]):
         if not node:
             return None, None
         node._propagate()
+        par = node._par
         u = k if node._left is None else k - node._left._size
         s, t = None, None
         if u == 0:
@@ -313,11 +99,26 @@ class WBTList(Generic[T, F]):
         else:
             s, t = self._split_node(node._right, u - 1)
             s = self._merge_with_root(node._left, node, s)
-        # if s:
-        #     s._par = None
-        # if t:
-        #     t._par = None
+        if s:
+            s._par = par
+        if t:
+            t._par = par
         return s, t
+
+    def find_order(self, k: int) -> "WBTList[T, F]":
+        if k < 0:
+            k += len(self)
+        node = self._root
+        while True:
+            node._propagate()
+            t = node._left._size if node._left else 0
+            if t == k:
+                return node
+            if t < k:
+                k -= t + 1
+                node = node._right
+            else:
+                node = node._left
 
     def split(self, k: int) -> tuple["WBTList", "WBTList"]:
         lnode, rnode = self._split_node(self._root, k)
@@ -354,28 +155,13 @@ class WBTList(Generic[T, F]):
         self._root = self._merge_node(s, t)
         return tmp._key
 
-    def tolist(self) -> list:
-        node = self._root
-        stack: list[_WBTListNode] = []
-        a = []
-        while stack or node:
-            if node:
-                node._propagate()
-                stack.append(node)
-                node = node._left
-            else:
-                node = stack.pop()
-                a.append(node._key)
-                node = node._right
-        return a
-
     def _check(self, verbose: bool = False) -> None:
         """作業用デバック関数
         size,key,balanceをチェックして、正しければ高さを表示する
         """
         if self._root is None:
             if verbose:
-                debug("ok. 0 (empty)")
+                print("ok. 0 (empty)")
             return
 
         # _size, height
@@ -396,12 +182,13 @@ class WBTList(Generic[T, F]):
             node._balance_check()
             return s, h + 1
 
+        assert self._root._par is None
         _, h = dfs(self._root)
         if verbose:
-            debug(f"ok. {h}")
+            print(f"ok. {h}")
 
     def prod(self, l: int, r: int) -> T:
-        def dfs(node: _WBTListNode, left: int, right: int) -> T:
+        def dfs(node: _WBTListNode[T, F], left: int, right: int) -> T:
             if right <= l or r <= left:
                 return self._e
             node._propagate()
@@ -434,115 +221,18 @@ class WBTList(Generic[T, F]):
     def __len__(self):
         return self._root._size if self._root else 0
 
+    def __iter__(self):
+        node = self._root
+        stack: list[_WBTListNode] = []
+        while stack or node:
+            if node:
+                node._propagate()
+                stack.append(node)
+                node = node._left
+            else:
+                node = stack.pop()
+                yield node._key
+                node = node._right
 
-def debug(*args):
-    pass
-
-
-import sys
-import os
-import io
-
-
-class FastO:
-    """標準出力高速化ライブラリです。"""
-
-    _output = io.StringIO()
-
-    @classmethod
-    def write(cls, *args, sep: str = " ", end: str = "\n", flush: bool = False) -> None:
-        """標準出力します。次の ``FastO.flush()`` が起きると print します。"""
-        wr = cls._output.write
-        for i in range(len(args) - 1):
-            wr(str(args[i]))
-            wr(sep)
-        if args:
-            wr(str(args[-1]))
-        wr(end)
-        if flush:
-            cls.flush()
-
-    @classmethod
-    def flush(cls) -> None:
-        """``flush`` します。これを実行しないと ``write`` した内容が表示されないので忘れないでください。"""
-        os.write(1, cls._output.getvalue().encode())
-        cls._output.close()
-
-
-write, flush = FastO.write, FastO.flush
-mod = 998244353
-
-
-def op(s, t):
-    s1, s2 = s >> 30, s & msk
-    t1, t2 = t >> 30, t & msk
-    c1 = (s1 + t1) % mod
-    c2 = (s2 + t2) % mod
-    return (c1 << 30) + c2
-
-
-def mapping(f, s):
-    f1, f2 = f >> 30, f & msk
-    s1, s2 = s >> 30, s & msk
-    return (((s1 * f1 + s2 * f2) % mod) << 30) + s2
-
-
-def composition(f, g):
-    f1, f2 = f >> 30, f & msk
-    g1, g2 = g >> 30, g & msk
-    z1 = (f1 * g1) % mod
-    z2 = (f1 * g2 + f2) % mod
-    return (z1 << 30) + z2
-
-
-e = 0
-id = 1 << 30
-msk = (1 << 30) - 1
-
-
-def dynamic_sequence():
-    input = lambda: sys.stdin.buffer.readline().rstrip()
-    n, q = map(int, input().split())
-    A = list(map(int, input().split()))
-    V = [a << 30 | 1 for a in A]
-
-    s = WBTList(op, mapping, composition, e, id, V)
-
-    for i in range(q):
-        com, *qu = map(int, input().split())
-        if com == 0:
-            i, x = qu
-            s.insert(i, x << 30 | 1)
-        elif com == 1:
-            i = qu[0]
-            s.pop(i)
-        elif com == 2:
-            l, r = qu
-            s.reverse(l, r)
-        elif com == 3:
-            l, r, b, c = qu
-            s.apply(l, r, (b << 30) | c)
-        else:
-            l, r = qu
-            write(s.prod(l, r) >> 30)
-    flush()
-
-
-def LR_insertion():
-    input = lambda: sys.stdin.readline().rstrip()
-    n = int(input())
-    s = input()
-    wbt = WBTList(op, mapping, composition, e, id, [0])
-    indx = 0
-    for i, c in enumerate(s, 1):
-        if c == "L":
-            wbt.insert(indx, i)
-        else:
-            indx += 1
-            wbt.insert(indx, i)
-    write(" ".join(map(str, wbt.tolist())))
-    flush()
-
-
-dynamic_sequence()
-# LR_insertion()
+    def __str__(self):
+        return str(list(self))
