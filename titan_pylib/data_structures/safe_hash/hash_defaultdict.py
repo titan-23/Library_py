e586aecfe5fc2f3858da18897dd7fd2bbaaefd04
titan-23 <titan.kaneda@gmail.com>
2024-06-06 13:37:56 +0900

    ハッシュテーブルの整形

--- titan_pylib/data_structures/safe_hash/hash_defaultdict.py
+++ titan_pylib/data_structures/safe_hash/hash_defaultdict.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
-import random
+from random import Random
+from typing import Final
 
 
 class HashDefaultdict:
 
-    _xor = random.randrange(10000000, 1000000000)
+    _r = Random()
+    _xor: Final[int] = _r.randrange(10000000, 1000000000)
 
     def __init__(self, missing):
         self._data = defaultdict(missing)
@@ -14,28 +16,28 @@ class HashDefaultdict:
             yield k, v
 
     def __setitem__(self, key: int, value):
-        self._data[key ^ HashDefaultdict._xor] = value
+        self._data[key ^ self._xor] = value
 
     def __getitem__(self, key: int):
-        return self._data[key ^ HashDefaultdict._xor]
+        return self._data[key ^ self._xor]
 
     def __delitem__(self, key: int):
-        del self._data[key ^ HashDefaultdict._xor]
+        del self._data[key ^ self._xor]
 
     def __contains__(self, item):
-        return item ^ HashDefaultdict._xor in self._data
+        return item ^ self._xor in self._data
 
     def __len__(self):
         return len(self._data)
 
     def keys(self):
-        return (k ^ HashDefaultdict._xor for k in self._data.keys())
+        return (k ^ self._xor for k in self._data.keys())
 
     def values(self):
         return (v for v in self._data.values())
 
     def items(self):
-        return ((k ^ HashDefaultdict._xor, v) for k, v in self._data.items())
+        return ((k ^ self._xor, v) for k, v in self._data.items())
 
     def __str__(self):
         return (
@@ -44,3 +46,6 @@ class HashDefaultdict:
             + ", ".join(f"{k}: {v}" for k, v in self.items())
             + "})"
         )
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self})"
