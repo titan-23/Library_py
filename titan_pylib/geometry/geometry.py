b844281cf21079bd730be6425d1dc208e4615949
titan-23 <titan.kaneda@gmail.com>
2024-06-13 06:24:33 +0900

    [Add] Geometry

--- titan_pylib/geometry/geometry.py
+++ titan_pylib/geometry/geometry.py
@@ -1,5 +1,10 @@
 import math
 from typing import Union, Optional
+from titan_pylib.algorithm.sort.merge_sort import merge_sort
+from decimal import Decimal
+
+# GEO_EPS = Decimal("1e-8")
+GEO_EPS = 1e-8
 
 
 class Point:
@@ -43,25 +48,37 @@ class Point:
         return Point(self.x / other, self.y / other)
 
     def __le__(self, other: "Point") -> bool:
-        return (self.x, self.y) <= (other.x, other.y)
+        if self == other:
+            return True
+        if Geometry._lt(self.x, other.x):
+            return True
+        if Geometry._lt(other.x, self.x):
+            return False
+        return Geometry._lt(self.y, other.y)
 
     def __lt__(self, other: "Point") -> bool:
-        return (self.x, self.y) < (other.x, other.y)
+        if self == other:
+            return False
+        if Geometry._lt(self.x, other.x):
+            return True
+        if Geometry._lt(other.x, self.x):
+            return False
+        return Geometry._lt(self.y, other.y)
 
     def __ge__(self, other: "Point") -> bool:
-        return (self.x, self.y) >= (other.x, other.y)
+        return not (self < other)
 
     def __gt__(self, other: "Point") -> bool:
-        return (self.x, self.y) > (other.x, other.y)
+        return not (self <= other)
 
     def __eq__(self, other: "Point") -> bool:
-        return (self.x, self.y) == (other.x, other.y)
+        return abs(self.x - other.x) < GEO_EPS and abs(self.y - other.y) < GEO_EPS
 
     def __neg__(self) -> "Point":
         return Point(-self.x, -self.y)
 
     def __abs__(self) -> float:
-        return math.sqrt(self.x**2 + self.y**2)
+        return math.sqrt(self.x * self.x + self.y * self.y)
 
     def norm(self) -> float:
         """norm"""
@@ -69,7 +86,7 @@ class Point:
 
     def norm2(self) -> float:
         """(x**2 + y**2)を返す(ルートの計算はしない)"""
-        return self.x**2 + self.y**2
+        return self.x * self.x + self.y * self.y
 
     def unit(self) -> "Point":
         return self / abs(self)
@@ -121,12 +138,12 @@ class Segment:
         self.p2 = p2
 
 
-class Porigon:
+class Polygon:
     """多角形クラス"""
 
     def __init__(self, ps: list[Point]) -> None:
         self.n = len(ps)
-        self.ps = ps
+        self.ps = sorted(Geometry.argumant_sort(ps))
 
     def area(self) -> float:
         """面積を求める / `O(n)`
@@ -169,7 +186,7 @@ class Porigon:
         """
         is_in = False
         ps = self.ps
-        EPS = Geometry.EPS
+        EPS = GEO_EPS
         for i in range(self.n):
             a = ps[i] - p
             b = ps[(i + 1) % self.n] - p
@@ -190,15 +207,17 @@ class Porigon:
         Returns:
             list[Point]: 凸包
         """
-        ps = sorted(self.ps, key=lambda p: (p.y, p.x))
+        ps = Geometry.sort_by_y(self.ps)
+        if not line_contains:
+            ps = Geometry.unique(ps)
         if len(ps) <= 1:
             return ps
 
         def cross(a, p):
             if line_contains:
-                return Geometry.cross(a[-1] - a[-2], p - a[-1]) < -Geometry.EPS
+                return Geometry.cross(a[-1] - a[-2], p - a[-1]) < -GEO_EPS
             else:
-                return Geometry.cross(a[-1] - a[-2], p - a[-1]) < Geometry.EPS
+                return Geometry.cross(a[-1] - a[-2], p - a[-1]) < GEO_EPS
 
         lower = []
         for p in ps:
@@ -213,6 +232,41 @@ class Porigon:
             upper.append(p)
 
         return lower[:-1] + upper[:-1]
+
+    def diameter(self) -> Optional[tuple[float, int, int]]:
+        ch = self.convex_hull()
+        n = len(ch)
+        if n == 0:
+            return None
+        if n == 1:
+            return 0, ch[0], ch[0]
+        if n == 2:
+            return abs(ch[0] - ch[1]), ch[0], ch[1]
+        u, v = 0, 0
+        up, vp = None, None
+        for i in range(n):
+            if ch[u] > ch[i]:
+                u = i
+                up = ch[i]
+            if ch[v] < ch[i]:
+                v = i
+                vp = ch[i]
+        d = 0
+        su, sv = u, v
+        loop = False
+        while (u != su or v != sv) or (not loop):
+            loop = True
+            if Geometry._lt(d, abs(ch[u] - ch[v])):
+                d = abs(ch[u] - ch[v])
+                up = ch[u]
+                vp = ch[v]
+            if Geometry._lt(
+                Geometry.cross(ch[(u + 1) % n] - ch[u], ch[(v + 1) % n] - ch[v]), 0
+            ):
+                u = (u + 1) % n
+            else:
+                v = (v + 1) % n
+        return d, up, vp
 
 
 class Circle:
@@ -238,13 +292,12 @@ class Geometry:
     """ref:
     - https://hcpc-hokudai.github.io/archive/geometry_004.pdf
     - https://bakamono1357.hatenablog.com/entry/2020/04/29/025320
+    - https://tjkendev.github.io/procon-library/python/geometry/circles_associated_with_triangle.html
     """
-
-    EPS = 1e-6
 
     @classmethod
     def _eq(cls, a: float, b: float) -> bool:
-        return abs(a - b) < cls.EPS
+        return abs(a - b) < GEO_EPS
 
     @classmethod
     def dot(cls, p1: Point, p2: Point) -> float:
@@ -270,13 +323,14 @@ class Geometry:
                  `-2`: a->bに対し、b->cが同一方向に進む
                  ` 0`: cが線分a->b上にある
         """
-        if cls.cross(v - u, p - u) > cls.EPS:
+        if cls.cross(v - u, p - u) > GEO_EPS:
             return 1
-        if cls.cross(v - u, p - u) < -cls.EPS:
+        if cls.cross(v - u, p - u) < -GEO_EPS:
             return -1
-        if cls.dot(v - u, p - u) < -cls.EPS:
+        if cls.dot(v - u, p - u) < -GEO_EPS:
             return 2
-        if abs(v - u) + cls.EPS < abs(p - u):
+        if Geometry._lt(abs(v - u), abs(p - u)):
+            # if abs(v - u) + GEO_EPS < abs(p - u):
             return -2
         return 0
 
@@ -366,13 +420,15 @@ class Geometry:
                  `4`: 離れている
         """
         d = abs(c1.p - c2.p)
-        if d > c1.r + c2.r + cls.EPS:
+        if Geometry._gt(d, c1.r + c2.r):
+            # if d > c1.r + c2.r + GEO_EPS:
             return 4
         if cls._eq(d, c1.r + c2.r):
             return 3
         if cls._eq(d, abs(c1.r - c2.r)):
             return 1
-        if d < abs(c1.r - c2.r) - cls.EPS:
+        if Geometry._lt(d, abs(c1.r - c2.r)):
+            # if d < abs(c1.r - c2.r) - GEO_EPS:
             return 0
         return 2
 
@@ -396,14 +452,16 @@ class Geometry:
             return (c1.p + (c2.p - c1.p) * t,)
         d = abs(c1.p - c2.p)
         if r == 1:  # 内接
-            if c2.r < c1.r - cls.EPS:
+            if Geometry._lt(c2.r, c1.r):
+                # if c2.r < c1.r - GEO_EPS:
                 return (c1.p + (c2.p - c1.p) * (c1.r / d),)
             else:
                 return (c2.p + (c1.p - c2.p) * (c2.r / d),)
         # 交点が2つ
         rcos = (c1.r * c1.r + d * d - c2.r * c2.r) / (2 * d)
         rsin = math.sqrt(c1.r * c1.r - rcos * rcos)
-        if c1.r - abs(rcos) < cls.EPS:
+        if Geometry._lt(c1.r - abs(rcos), 0):
+            # if c1.r - abs(rcos) < GEO_EPS:
             rsin = 0
         e12 = (c2.p - c1.p) / abs(c2.p - c1.p)
         return tuple(
@@ -419,7 +477,7 @@ class Geometry:
     def cross_point_circle_line(cls, c: Circle, l: Line) -> list[Point]:
         res = []
         d = cls.dist_p_to_line(c.p, l)
-        if d > c.r + cls.EPS:
+        if d > c.r + GEO_EPS:
             return res
         h = cls.projection_point(c.p, l)
         if cls._eq(d, c.r):
@@ -438,9 +496,9 @@ class Geometry:
 
     @classmethod
     def dist_p_to_segmemt(cls, p: Point, s: Segment) -> float:
-        if cls.dot(s.p2 - s.p1, p - s.p1) < cls.EPS:
+        if cls.dot(s.p2 - s.p1, p - s.p1) < GEO_EPS:
             return abs(p - s.p1)
-        if cls.dot(s.p1 - s.p2, p - s.p2) < cls.EPS:
+        if cls.dot(s.p1 - s.p2, p - s.p2) < GEO_EPS:
             return abs(p - s.p2)
         return abs(cls.cross(s.p2 - s.p1, p - s.p1)) / abs(s.p2 - s.p1)
 
@@ -462,6 +520,59 @@ class Geometry:
         p /= a + b + c
         d = cls.dist_p_to_segmemt(p, Line.from_points(p1, p2))
         return Circle(p, d)
+
+    @classmethod
+    def triangle_circumcircle(cls, p1: Point, p2: Point, p3: Point) -> Circle:
+        a = 2 * (p1.x - p2.x)
+        b = 2 * (p1.y - p2.y)
+        p = p1.x**2 - p2.x**2 + p1.y**2 - p2.y**2
+        c = 2 * (p1.x - p3.x)
+        d = 2 * (p1.y - p3.y)
+        q = p1.x**2 - p3.x**2 + p1.y**2 - p3.y**2
+        u = a * d - b * c
+        x = d * p - b * q
+        y = a * q - c * p
+        if Geometry._lt(u, 0):
+            x = -x
+            y = -y
+            u = -u
+        x /= u
+        y /= u
+        r = ((x - p1.x) ** 2 + (y - p1.y) ** 2) ** 0.5
+        return Circle(Point(x, y), r)
+
+    @classmethod
+    def triangle_excircle(
+        cls, p1: Point, p2: Point, p3: Point
+    ) -> tuple[Circle, Circle, Circle]:
+        dx1 = p2.x - p1.x
+        dy1 = p2.y - p1.y
+        dx2 = p3.x - p1.x
+        dy2 = p3.y - p1.y
+        d1 = ((p3.x - p2.x) ** 2 + (p3.y - p2.y) ** 2) ** 0.5
+        d3 = (dx1**2 + dy1**2) ** 0.5
+        d2 = (dx2**2 + dy2**2) ** 0.5
+        S2 = abs(dx1 * dy2 - dx2 * dy1)
+        dsum1 = -d1 + d2 + d3
+        r1 = S2 / dsum1
+        ex1 = (-p1.x * d1 + p2.x * d2 + p3.x * d3) / dsum1
+        ey1 = (-p1.y * d1 + p2.y * d2 + p3.y * d3) / dsum1
+
+        dsum2 = d1 - d2 + d3
+        r2 = S2 / dsum2
+        ex2 = (p1.x * d1 - p2.x * d2 + p3.x * d3) / dsum2
+        ey2 = (p1.y * d1 - p2.y * d2 + p3.y * d3) / dsum2
+
+        dsum3 = d1 + d2 - d3
+        r3 = S2 / dsum3
+        ex3 = (p1.x * d1 + p2.x * d2 - p3.x * d3) / dsum3
+        ey3 = (p1.y * d1 + p2.y * d2 - p3.y * d3) / dsum3
+
+        return (
+            Circle(Point(ex1, ey1), r1),
+            Circle(Point(ex2, ey2), r2),
+            Circle(Point(ex3, ey3), r3),
+        )
 
     @classmethod
     def circle_tangent(cls, p: Point, c: Circle) -> tuple[Point, Point]:
@@ -497,16 +608,170 @@ class Geometry:
                 )
         return ret
 
+    @classmethod
+    def unique(cls, a: list[Point]) -> list[Point]:
+        """同一要素の削除
 
-c1x, c1y, c1r = map(int, input().split())
-c2x, c2y, c2r = map(int, input().split())
-c1 = Circle(Point(c1x, c1y), c1r)
-c2 = Circle(Point(c2x, c2y), c2r)
-tangents = Geometry.circle_common_tangent(c1, c2)
-ans = []
-for line in tangents:
-    ps = Geometry.cross_point_circle_line(c1, line)
-    ans.extend(ps)
-ans.sort()
-for p in ans:
-    p.show()
+        Note:
+            `a` は事前にソートされていなければいけません。
+
+        Args:
+            a (list[Point]):
+
+        Returns:
+            list[Point]:
+        """
+        if not a:
+            return []
+        res = [a[0]]
+        for i in range(1, len(a)):
+            if a[i] == res[-1]:
+                continue
+            res.append(a[i])
+        return res
+
+    @classmethod
+    def _lt(cls, u: float, v: float) -> bool:
+        return u < v and u < max(v, v - GEO_EPS)
+
+    @classmethod
+    def _gt(cls, u: float, v: float) -> bool:
+        return u > v and u > min(v, v - GEO_EPS)
+
+    @classmethod
+    def argumant_sort(cls, a: list[Point]) -> list[Point]:
+        upper = []
+        lower = []
+        zeros = []
+        for p in a:
+            if cls._lt(p.y, 0) or (cls._eq(p.y, 0) and cls._gt(p.x, 0)):
+                lower.append(p)
+            elif cls._eq(p.x, 0) and cls._eq(p.y, 0):
+                zeros.append(p)
+            else:
+                upper.append(p)
+
+        def cmp(s: Point, t: Point) -> bool:
+            return cls._lt(s.y * t.x, s.x * t.y)
+
+        upper = merge_sort(upper, key=cmp)
+        lower = merge_sort(lower, key=cmp)
+        return lower + zeros + upper
+
+    @classmethod
+    def sort_by_x(cls, a: list[Point]) -> list[Point]:
+        return sorted(a)
+
+    @classmethod
+    def _cmp_y(cls, u: Point, v: Point):
+        if cls._eq(u, v):
+            return True
+        if cls._lt(u.y, v.y):
+            return True
+        if cls._lt(v.y, u.y):
+            return False
+        return cls._lt(u.x, v.x)
+
+    @classmethod
+    def sort_by_y(cls, a: list[Point]) -> list[Point]:
+        return merge_sort(a, cls._cmp_y)
+
+    @classmethod
+    def furthest_pair(cls, a: list[Point]) -> Optional[tuple[float, int, int]]:
+        p = Polygon(a)
+        d, up, vp = p.diameter()
+        ui, vi = -1, -1
+        for i, point in enumerate(a):
+            if point == up:
+                ui = i
+        for i, point in enumerate(a):
+            if i != ui and point == vp:
+                vi = i
+        assert ui != -1 and vi != -1
+        return d, ui, vi
+
+    @classmethod
+    def closest_pair(cls, a: list[Point]) -> Optional[tuple[float, int, int]]:
+        """最近点対を求める / O(nlogn)
+
+        Args:
+            a (list[Point]): 距離配列
+
+        Returns:
+            Optional[tuple[float, int, int]]: 距離、最近点対
+        """
+
+        buff = [0] * len(a)
+        d = float("inf")
+        p, q = -1, -1
+
+        def f(l: int, r: int) -> None:
+            nonlocal d, p, q
+            mid = (l + r) // 2
+            x = a[mid][0].x
+            if mid - l > 1:
+                f(l, mid)
+            if r - mid > 1:
+                f(mid, r)
+            i, j = l, mid
+            indx = 0
+            while i < mid and j < r:
+                if cls._cmp_y(a[i][0], a[j][0]):
+                    buff[indx] = a[i]
+                    i += 1
+                else:
+                    buff[indx] = a[j]
+                    j += 1
+                indx += 1
+            for i in range(i, mid):
+                buff[indx] = a[i]
+                indx += 1
+            for j in range(j, r):
+                buff[indx] = a[j]
+                indx += 1
+            a[l:r] = buff[: r - l]
+
+            near_line = []
+            for i in range(l, r):
+                e_p, e_i = a[i]
+                v = abs(e_p.x - x)
+                if cls._gt(v, d) or cls._eq(v, d):
+                    continue
+                for ne_p, ne_i in reversed(near_line):
+                    dx = e_p.x - ne_p.x
+                    dy = e_p.y - ne_p.y
+                    if cls._gt(dy, d) or cls._eq(dy, d):
+                        break
+                    cand = math.sqrt(dx * dx + dy * dy)
+                    if cls._lt(cand, d):
+                        d = cand
+                        p, q = ne_i, e_i
+                near_line.append(a[i])
+
+        if len(a) <= 1:
+            return None
+        a = sorted((e, i) for i, e in enumerate(a))
+        f(0, len(a))
+        return d, p, q
+
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+#  -----------------------  #
+
+
+def solve():
+    n = int(input())
+    a = []
+    for _ in range(n):
+        x, y = map(int, input().split())
+        a.append(Point(x, y))
+    d, ui, vi = Geometry.furthest_pair(a)
+    print(ui, vi)
+
+
+t = int(input())
+for _ in range(t):
+    solve()
