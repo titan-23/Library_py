dd618e9478ead2dffa5b4bc877b5b57f70600d33
titan-23 <titan.kaneda@gmail.com>
2024-06-18 22:47:17 +0900

    decimal_pi docs

--- titan_pylib/math/decimal_util.py
+++ titan_pylib/math/decimal_util.py
@@ -1,7 +1,8 @@
 from decimal import Decimal, getcontext
+import math
 
 
-def decimal_pi():
+def decimal_pi() -> Decimal:
     getcontext().prec += 2
     three = Decimal(3)
     lasts, t, s, n, na, d, da = 0, three, 3, 1, 0, 0, 24
@@ -15,7 +16,7 @@ def decimal_pi():
     return +s
 
 
-def decimal_exp(x):
+def decimal_exp(x: Decimal) -> Decimal:
     getcontext().prec += 2
     i, lasts, s, fact, num = 0, 0, 1, 1, 1
     while s != lasts:
@@ -28,7 +29,7 @@ def decimal_exp(x):
     return +s
 
 
-def decimal_cos(x):
+def decimal_cos(x: Decimal) -> Decimal:
     getcontext().prec += 2
     i, lasts, s, fact, num, sign = 0, 0, 1, 1, 1, 1
     while s != lasts:
@@ -42,7 +43,7 @@ def decimal_cos(x):
     return +s
 
 
-def decimal_sin(x):
+def decimal_sin(x: Decimal) -> Decimal:
     getcontext().prec += 2
     i, lasts, s, fact, num, sign = 1, 0, x, 1, x, 1
     while s != lasts:
@@ -56,25 +57,51 @@ def decimal_sin(x):
     return +s
 
 
-def decimal_acos(x):
+def decimal_asin(x: Decimal) -> Decimal:
+    assert isinstance(x, Decimal)
+    if x < -1 or x > 1:
+        raise ValueError("math domain error")
+    if x == 1:
+        return decimal_pi() / 2
+    if x == -1:
+        return -decimal_pi() / 2
+    getcontext().prec += 2
+    eps = Decimal("1e-" + str(getcontext().prec))
+    sum_acos = x
+    term = Decimal(1)
+    power = x
+    n = 0
+    while abs(term * power) > eps:
+        n += 1
+        term *= (2 * n - 1) * (2 * n) * (2 * (n - 1) + 1)
+        term /= 4 * n * n * (2 * n + 1)
+        power *= x * x
+        sum_acos += term * power
+    result = sum_acos
+    getcontext().prec -= 2
+    return +result
+
+
+def decimal_acos(x: Decimal) -> Decimal:
+    assert isinstance(x, Decimal)
     if x < -1 or x > 1:
         raise ValueError("math domain error")
     if x == 1:
         return Decimal(0)
     if x == -1:
         return decimal_pi()
-    epsilon = Decimal("1e-" + str(getcontext().prec))
     getcontext().prec += 2
-    sum_acos = Decimal(0)
-    term = x
-    power = x**2
+    eps = Decimal("1e-" + str(getcontext().prec))
+    sum_acos = x
+    term = Decimal(1)
+    power = x
     n = 0
-    while abs(term) > epsilon:
+    while abs(term * power) > eps:
         n += 1
-        term *= Decimal((2 * n - 1) * (2 * n)) / (4 * n * n * (2 * n + 1))
-        term *= power
-        sum_acos += term
-        power *= x**2
+        term *= (2 * n - 1) * (2 * n) * (2 * (n - 1) + 1)
+        term /= 4 * n * n * (2 * n + 1)
+        power *= x * x
+        sum_acos += term * power
     result = decimal_pi() / 2 - sum_acos
     getcontext().prec -= 2
     return +result
