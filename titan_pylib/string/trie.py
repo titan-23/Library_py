fdcf7a0f2f48f7dc0c0a75899776f5491a6cc69b
titanium-22 <titan.kaneda@gmail.com>
2025-04-28 13:33:53 +0900

    Fix trie

--- titan_pylib/string/trie.py
+++ titan_pylib/string/trie.py
@@ -1,5 +1,7 @@
 class Trie:
+
     class Node:
+
         def __init__(self):
             self.c = None
             self.child = {}
@@ -20,6 +22,7 @@ class Trie:
         node.stop_count += 1
 
     def s_prefix(self, pref) -> int:
+        """?"""
         node = self.root
         for dep, c in enumerate(pref):
             if c not in node.child:
@@ -27,7 +30,65 @@ class Trie:
             node = node.child[c]
         return len(pref)
 
+    def erase(self, s) -> None:
+        """sがあればsを一つ削除する / O(|s|)"""
+        if s not in self:
+            return
+        node = self.root
+        for c in s:
+            node.count -= 1
+            node = node.child[c]
+        node.stop_count -= 1
+
+    def erase_prefix(self, pref) -> int:
+        """prefを接頭辞に持つすべての文字列を削除し、削除した文字列の個数を返す / O(|pref|)"""
+        node = self.root
+        for c in pref:
+            if c not in node.child:
+                return 0
+            node = node.child[c]
+        remove_cnt = node.count + node.stop_count
+        node = self.root
+        if node.count - remove_cnt == 0:  # 全部消える
+            self.root = Trie.Node()
+            return remove_cnt
+        for c in pref:
+            node.count -= remove_cnt
+            assert node.count > 0
+            assert c in node.child
+            if node.child[c].count - remove_cnt == 0:
+                del node.child[c]
+                return remove_cnt
+            node = node.child[c]
+        node.count = 0
+        node.stop_count = 0
+        node.child.clear()
+        return remove_cnt
+
+    def contains_prefix(self, pref: str) -> bool:
+        """prefを接頭辞に持つ文字列があるかどうか / O(|pref|)"""
+        node = self.root
+        for c in pref:
+            if c not in node.child:
+                return False
+            node = node.child[c]
+        return True
+
+    def contains_prefix_inv(self, s: str) -> bool:
+        """sの接頭辞になり得る文字列が存在しているか / O(|s|)"""
+        node = self.root
+        for c in s:
+            if c not in node.child:
+                return False
+            if node.stop_count > 0:
+                return True
+            node = node.child[c]
+            if node.stop_count > 0:
+                return True
+        return False
+
     def count(self, s: str) -> int:
+        """sがいくつ含まれているか"""
         node = self.root
         for dep, c in enumerate(s):
             if c not in node.child:
