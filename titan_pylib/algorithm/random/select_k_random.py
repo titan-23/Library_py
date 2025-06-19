c60f5a838ebed10887ffc1bd54c2034b96e34277
titan-23 <titan.kaneda@gmail.com>
2025-06-19 14:24:09 +0900

    add random.sample

---
+++ titan_pylib/algorithm/random/select_k_random.py
@@ -0,0 +1,22 @@
+import random
+
+
+def select_k_random(n: int, k: int) -> list[int]:
+    if k > n or k < 0:
+        raise ValueError("k must be between 0 and n")
+    if k < n // 3:
+        result = set()
+        for j in range(n - k, n):
+            t = random.randrange(j + 1)
+            if t not in result:
+                result.add(t)
+            else:
+                result.add(j)
+        return sorted(result)
+    else:
+        result = list(range(1, k + 1))
+        for i in range(k, n):
+            r = random.randint(0, i)
+            if r < k:
+                result[r] = i + 1
+        return sorted(result)
